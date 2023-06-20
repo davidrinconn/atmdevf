7d1a458ba7c0f2e4fb7bb25a7b02391bacfa52f4
David Rincon <david.rincon.ayala@gmail.com>
2023-06-19 21:47:11 -0600

    atmdevf

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+//function sum(){
+  //  return 4 + 6
+//}
+
+//console.log(sum())
+
+function sum(value1, value2){
+        return value1 + value2
+}
+
+console.log(sum(8, 7))
+console.log(sum(15, 35))
